0f343a4abbaab5db4c56517bb356f9f11f2596c8
Tiago Dias Provenzano <31420080+tiagodprovenzano@users.noreply.github.com>
2021-03-27 16:04:16 -0300

    edited script

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.entities = void 0;
+var src_1 = require("./src");
+Object.defineProperty(exports, "entities", { enumerable: true, get: function () { return src_1.entities; } });
